9921e5336e42b192df704b26e3282daec6b16754
Jannik Volkland <volkland@sipgate.de>
2024-10-15 16:55:02 +0200

    feat: add X1Version utility
    Co-authored-by: Sven Uhlig <uhlig@sipgate.de>

---
+++ src/main/java/com/sipgate/li/lib/x1/X1Version.java
@@ -0,0 +1,20 @@
+package com.sipgate.li.lib.x1;
+
+public class X1Version {
+
+  private static final String MAJOR_VERSION = "v1";
+  public static final String VERSION = MAJOR_VERSION + ".15.1";
+
+  /**
+   * Only major version changes break compatibility. See: TS 103.221-1, Section 4.5 Release Management.
+   */
+  public static boolean isCompatible(final String candidateVersion) {
+    if (null == candidateVersion || !candidateVersion.contains(".")) {
+      return false;
+    }
+
+    final var majorVersion = (candidateVersion.substring(0, candidateVersion.indexOf(".")));
+
+    return MAJOR_VERSION.equals(majorVersion);
+  }
+}
