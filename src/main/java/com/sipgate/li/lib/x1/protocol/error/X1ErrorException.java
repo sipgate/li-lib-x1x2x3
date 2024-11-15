b346abe76f26968c79a3427aa23006f9be773164
Sven Uhlig <uhlig@sipgate.de>
2024-11-15 19:48:42 +0100

    refactor(x1): make error codes public for testing

--- src/main/java/com/sipgate/li/lib/x1/protocol/error/X1ErrorException.java
+++ src/main/java/com/sipgate/li/lib/x1/protocol/error/X1ErrorException.java
@@ -4,27 +4,27 @@ import java.math.BigInteger;
 
 public abstract class X1ErrorException extends Exception {
 
-  protected static final BigInteger GENERIC_ERROR = BigInteger.valueOf(1000);
-  protected static final BigInteger SYNTAX_SCHEMA_ERROR = BigInteger.valueOf(1010);
-  protected static final BigInteger UNSUPPORTED_VERSION = BigInteger.valueOf(1020);
-  protected static final BigInteger UNSUPPORTED_REQUEST = BigInteger.valueOf(1080);
+  public static final BigInteger GENERIC_ERROR = BigInteger.valueOf(1000);
+  public static final BigInteger SYNTAX_SCHEMA_ERROR = BigInteger.valueOf(1010);
+  public static final BigInteger UNSUPPORTED_VERSION = BigInteger.valueOf(1020);
+  public static final BigInteger UNSUPPORTED_REQUEST = BigInteger.valueOf(1080);
 
-  protected static final BigInteger XID_ALREADY_EXISTS = BigInteger.valueOf(2010);
-  protected static final BigInteger XID_DOES_NOT_EXIST = BigInteger.valueOf(2020);
-  protected static final BigInteger DID_ALREADY_EXISTS = BigInteger.valueOf(2030);
-  protected static final BigInteger DID_DOES_NOT_EXIST = BigInteger.valueOf(2040);
+  public static final BigInteger XID_ALREADY_EXISTS = BigInteger.valueOf(2010);
+  public static final BigInteger XID_DOES_NOT_EXIST = BigInteger.valueOf(2020);
+  public static final BigInteger DID_ALREADY_EXISTS = BigInteger.valueOf(2030);
+  public static final BigInteger DID_DOES_NOT_EXIST = BigInteger.valueOf(2040);
 
-  protected static final BigInteger GENERIC_ACTIVATETASK_FAILURE = BigInteger.valueOf(3000);
-  protected static final BigInteger GENERIC_MODIFYTASK_FAILURE = BigInteger.valueOf(3001);
-  protected static final BigInteger INVALID_COMBINATION_OF_DELIVERYTYPE_AND_DESTINATIONS = BigInteger.valueOf(3040);
+  public static final BigInteger GENERIC_ACTIVATETASK_FAILURE = BigInteger.valueOf(3000);
+  public static final BigInteger GENERIC_MODIFYTASK_FAILURE = BigInteger.valueOf(3001);
+  public static final BigInteger INVALID_COMBINATION_OF_DELIVERYTYPE_AND_DESTINATIONS = BigInteger.valueOf(3040);
 
-  protected static final BigInteger GENERIC_DEACTIVATETASK_FAILURE = BigInteger.valueOf(4000);
+  public static final BigInteger GENERIC_DEACTIVATETASK_FAILURE = BigInteger.valueOf(4000);
 
-  protected static final BigInteger GENERIC_CREATEDESTINATION_FAILURE = BigInteger.valueOf(6000);
-  protected static final BigInteger GENERIC_MODIFYDESTINATION_FAILURE = BigInteger.valueOf(6001);
+  public static final BigInteger GENERIC_CREATEDESTINATION_FAILURE = BigInteger.valueOf(6000);
+  public static final BigInteger GENERIC_MODIFYDESTINATION_FAILURE = BigInteger.valueOf(6001);
 
-  protected static final BigInteger GENERIC_REMOVEDESTINATION_FAILURE = BigInteger.valueOf(7000);
-  protected static final BigInteger DESTINATION_IN_USE = BigInteger.valueOf(7010);
+  public static final BigInteger GENERIC_REMOVEDESTINATION_FAILURE = BigInteger.valueOf(7000);
+  public static final BigInteger DESTINATION_IN_USE = BigInteger.valueOf(7010);
 
   private final BigInteger errorCode;
   private final String errorDescription;
